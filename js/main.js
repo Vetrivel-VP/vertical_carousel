cbd6363b4e913a1886b655daf54991600dc9cae1
VETRIVEL <vetrivel.galaxy@gmail.com>
2021-11-07 18:08:17 +0300

    Add : Vertical Carousel Added

---
+++ js/main.js
@@ -0,0 +1,68 @@
+var cards = $("#card_slider .slider_item").toArray();
+
+startAnim(cards);
+
+function startAnim(array) {
+  if (array.length >= 4) {
+    TweenMax.fromTo(
+      array[0],
+      0.5,
+      { x: 0, y: 0, opacity: 0.75 },
+      {
+        x: 0,
+        y: -120,
+        opacity: 0,
+        zIndex: 0,
+        delay: 0.03,
+        ease: Cubic.easeInOut,
+        onComplete: sortArray(array),
+      }
+    );
+
+    TweenMax.fromTo(
+      array[1],
+      0.5,
+      { x: 79, y: 125, opacity: 1, zIndex: 1 },
+      {
+        x: 0,
+        y: 0,
+        opacity: 0.75,
+        zIndex: 0,
+        boxShadow: "-5px 8px 8px 0 rgba(82,89,129,0.05)",
+        ease: Cubic.easeInOut,
+      }
+    );
+
+    TweenMax.to(array[2], 0.5, {
+      bezier: [
+        { x: 0, y: 250 },
+        { x: 65, y: 200 },
+        { x: 79, y: 125 },
+      ],
+      boxShadow: "-5px 8px 8px 0 rgba(82,89,129,0.05)",
+      zIndex: 1,
+      opacity: 1,
+      ease: Cubic.easeInOut,
+    });
+
+    TweenMax.fromTo(
+      array[3],
+      0.5,
+      { x: 0, y: 400, opacity: 0, zIndex: 0 },
+      { x: 0, y: 250, opacity: 0.75, zIndex: 0, ease: Cubic.easeInOut }
+    );
+  } else {
+    $("#card_slider").append(
+      "<p>Sorry, carousel should contain more than 3 slides</p>"
+    );
+  }
+}
+
+function sortArray(array) {
+  clearTimeout(delay);
+  var delay = setTimeout(function () {
+    var firstElem = array.shift();
+    array.push(firstElem);
+    return startAnim(array);
+  }, 3000);
+}
